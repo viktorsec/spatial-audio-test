828123b26440c70fc125cf2728e9d0a4f14afb14
Viktor Sec <hey@viktorsec.com>
2021-08-14 01:35:01 +0200

    Add music node

--- SpatialAudioTest/PlayerScene.swift
+++ SpatialAudioTest/PlayerScene.swift
@@ -9,16 +9,38 @@ import Foundation
 import SpriteKit
 
 class PlayerScene: SKScene {
+    
+    var node = SKSpriteNode()
+    var nodePosition = CGPoint()
+    var music = SKAudioNode(fileNamed: "jazzyfrenchy.mp3")
+    
+    
     override func didMove(to view: SKView) {
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
-        let box = SKSpriteNode(color: SKColor.red, size: CGSize(width: 50, height: 50))
-        box.position = location
-        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
-        addChild(box)
+        
+        node = SKSpriteNode(color: SKColor.red, size: CGSize(width: 50, height: 50))
+        node.position = location
+        addChild(node)
+        
+        addChild(music)
+        music.isPositional = true
+        music.position = location
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        if let location = touch?.location(in: self) {
+            node.run(SKAction.move(to: location, duration: 0))
+            music.position = location
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        node.removeFromParent()
+        music.removeFromParent()
     }
 }
