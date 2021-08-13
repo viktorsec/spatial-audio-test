d8f947b1a58d9fb32611bba95d031fae4e7e472f
Viktor Sec <hey@viktorsec.com>
2021-08-14 01:01:04 +0200

    Create SwiftUI app

---
+++ SpatialAudioTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpatialAudioTest
+//
+//  Created by Viktor Sec on 14/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpatialAudioTest/SpatialAudioTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialAudioTestApp.swift
+//  SpatialAudioTest
+//
+//  Created by Viktor Sec on 14/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpatialAudioTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
